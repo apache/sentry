77526056556f5785ab9b15206e7d3152b5d46189
Shreepadma Venugopalan <shreepadma@cloudera.com>
2013-03-18 11:49:05 -0700

    ACCESS-15. End to end test infrastructure

---
+++ access-tests/src/test/java/org/apache/access/tests/e2e/TestHiveAccess.java
@@ -0,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.access.tests.e2e;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import junit.framework.TestCase;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestHiveAccess {
+    private static final String driverName = "org.apache.hive.jdbc.HiveDriver";
+    private final HiveConf conf = null;
+    private boolean standAloneServer = false;
+    private Connection adminCon;
+    private Statement adminStmt;
+    private final String dataFileDir = "src/test/resources";
+    
+    public TestHiveAccess() {
+	
+    }
+    
+    private Connection getConnection(String username, String password) throws Exception {
+       Class.forName(driverName);
+       if (standAloneServer) {
+        // get connection
+	return DriverManager.getConnection("jdbc:hive2://localhost:10000/default",
+		      username, password);
+      } else {
+        return DriverManager.getConnection("jdbc:hive2://", username, password);
+      }
+    }
+    
+    public void setUpHS2() throws Exception {
+     
+      adminCon = getConnection("admin", "admin");
+      assertNotNull("Connection is null", adminCon);
+      assertFalse("Connection should not be closed", adminCon.isClosed());
+      
+      adminStmt  = adminCon.createStatement();
+      assertNotNull("Statement is null", adminStmt);
+      
+      adminStmt.execute("show tables");
+    }
+    
+    public void setUpAccess() throws Exception {
+      adminStmt.execute("set hive.support.concurrency = false");
+      adminStmt.execute("set hive.semantic.analyzer.hook = org.apache.access.binding.hive.HiveAuthzBindingHook");
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+	
+      /* To run end to end tests against HS2 from within the Access project,  
+       * 1. Setup Embedded HS2 and JDBC driver to talk to HS2
+       * 2. Set the semantic analyzer hooks to the hive binding class in the access project
+       * 3. Setup the access-site and policy files for the access provider
+       */
+     setUpHS2();
+     setUpAccess();
+    }
+    
+    @Test
+    public void test() throws Exception {
+      
+      String tableName = "Test_Tab007";
+      String tableComment = "Test table";
+      
+      // per test setup
+      Connection userCon = getConnection("foo", "foo");
+      Statement userStmt = userCon.createStatement();
+      Path dataFilePath = new Path(dataFileDir, "kv1.txt");
+      
+      // drop table. ignore error.
+      /*try {
+	stmt.execute("drop table " + tableName);
+      } catch (Exception ex) {
+        fail(ex.toString());
+      }*/
+
+      // create table 
+      userStmt.execute("create table " + tableName
+		   + " (under_col int comment 'the under column', value string) comment '"
+	           + tableComment + "'");
+      //TODO: LOAD, QUERY, DROP table fails. See ACCESS-30.
+      // load data
+      //stmt.execute("load data local inpath '" + dataFilePath.toString() + "' into table " + tableName);      
+      // query table
+      //ResultSet   res = stmt.executeQuery("select under_col, value from " + tableName);
+      //drop table
+      //stmt.execute("drop table " + tableName);
+      
+      
+      // per test tear down
+      userStmt.close();
+      userCon.close();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+      adminStmt.close();
+      adminCon.close();
+    }
+}
