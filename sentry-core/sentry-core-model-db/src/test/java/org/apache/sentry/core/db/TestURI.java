1001b70e11940d103ca5b38be9962abcbd3dda79
Sravya Tirukkovalur <sravya@clouera.com>
2013-11-01 16:48:06 -0700

    SENTRY-58: Fix compilation error in TestURI.java (Shreepadma Venugopalan via Sravya Tirukkovalur)

--- sentry-core/sentry-core-model-db/src/test/java/org/apache/sentry/core/db/TestURI.java
+++ sentry-core/sentry-core-model-db/src/test/java/org/apache/sentry/core/db/TestURI.java
@@ -1,4 +1,4 @@
-package org.apache.sentry.core;
+package org.apache.sentry.core.db;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
