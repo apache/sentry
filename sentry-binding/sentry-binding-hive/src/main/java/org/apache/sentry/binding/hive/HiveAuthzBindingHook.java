58be76016768fdb38f2cdb7e4b0acd7d8e468d0d
Prasad Mujumdar <prasadm@cloudera.com>
2014-09-17 09:36:49 -0700

    SENTRY-441: Improve the message for SemanticException (Sravya Tirukkovalur via Prasad Mujumdar)

--- sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/HiveAuthzBindingHook.java
+++ sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/HiveAuthzBindingHook.java
@@ -317,7 +317,9 @@ public class HiveAuthzBindingHook extends AbstractSemanticAnalyzerHook {
         permsRequired += perm + ";";
       }
       SessionState.get().getConf().set(HiveAuthzConf.HIVE_SENTRY_AUTH_ERRORS, permsRequired);
-      throw new SemanticException(HiveAuthzConf.HIVE_SENTRY_PRIVILEGE_ERROR_MESSAGE, e);
+      String msg = HiveAuthzConf.HIVE_SENTRY_PRIVILEGE_ERROR_MESSAGE + "\n Required privileges for this query: "
+          + permsRequired;
+      throw new SemanticException(msg, e);
     }
     if ("true".equalsIgnoreCase(context.getConf().
         get(HiveAuthzConf.HIVE_SENTRY_MOCK_COMPILATION))) {
