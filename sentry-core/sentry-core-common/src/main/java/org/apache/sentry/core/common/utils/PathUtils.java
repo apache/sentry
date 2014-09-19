f7f40b94428181bd06431f1bc60118d052f40f12
Prasad Mujumdar <prasadm@cloudera.com>
2014-09-19 15:27:57 -0700

    SENTRY-452: Uri tests failing on real cluster (Sravya Tirukkovalur via Prasad Mujumdar)

--- sentry-core/sentry-core-common/src/main/java/org/apache/sentry/core/common/utils/PathUtils.java
+++ sentry-core/sentry-core-common/src/main/java/org/apache/sentry/core/common/utils/PathUtils.java
@@ -109,6 +109,8 @@ public class PathUtils {
     } else {
       if (uri.startsWith("file:")) {
         uri = uri.replace("file:", "file://");
+      } if (uri.startsWith("hdfs:")) {
+        uri = uri.replace("hdfs:", "hdfs://");
       } else if (uri.startsWith("/")) {
         if (warehouseDir.startsWith("hdfs:")) {
           URI warehouse = toDFSURI(warehouseDir);
