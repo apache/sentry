692ff834e2059ec2dec2ab35f05f5b77c207bc20
Sravya Tirukkovalur <sravya@clouera.com>
2014-09-19 15:01:04 -0700

    SENTRY-451: CAST is still broken after SENTRY-118 patch (Grisha Trubetskoy via Sravya Tirukkovalur)

--- sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/conf/HiveAuthzConf.java
+++ sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/conf/HiveAuthzConf.java
@@ -108,6 +108,7 @@ public class HiveAuthzConf extends Configuration {
     "date,decimal,timestamp," + // SENTRY-312
     "abs,acos,and,array,array_contains,ascii,asin,assert_true,atan,avg," +
     "between,bin,case,cast,ceil,ceiling,coalesce,collect_set,compute_stats,concat,concat_ws," +
+    "UDFConv,UDFHex,UDFSign,UDFToBoolean,UDFToByte,UDFToDouble,UDFToFloat,UDFToInteger,UDFToLong,UDFToShort,UDFToString," +
     "context_ngrams,conv,corr,cos,count,covar_pop,covar_samp,create_union,date_add,date_sub," +
     "datediff,day,dayofmonth,degrees,div,e,elt,ewah_bitmap,ewah_bitmap_and,ewah_bitmap_empty," +
     "ewah_bitmap_or,exp,explode,field,find_in_set,floor,format_number,from_unixtime," +
