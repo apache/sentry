124b6f5ca4b79765d25f3630af040b5ee58326a6
Guoquan Shen <guoquan.shen@intel.com>
2015-02-26 12:37:51 +0800

    SENTRY-624: Add upgrade scripts for general privilege model (Reviewed by Colin Ma)

--- sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-postgres-1.4.0-to-1.5.0.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-postgres-1.4.0-to-1.5.0.sql
@@ -3,6 +3,7 @@ SELECT 'Upgrading Sentry store schema from 1.4.0 to 1.5.0';
 \i 002-SENTRY-339.postgres.sql;
 \i 003-SENTRY-380.postgres.sql;
 \i 004-SENTRY-74.postgres.sql;
+\i 005-SENTRY-398.postgres.sql;
 
 UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.5.0', VERSION_COMMENT='Sentry release version 1.5.0' WHERE VER_ID=1;
 SELECT 'Finished upgrading Sentry store schema from 1.4.0 to 1.5.0';
