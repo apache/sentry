9195cb9f512579bfd56a6894319ce739332b4cff
Prasad Mujumdar <prasadm@cloudera.com>
2015-04-24 23:51:59 -0700

    SENTRY-692: Add schema creation scripts for 1.6.0 version (Colin Ma via Prasad Mujumdar)

---
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-mysql-1.5.0-to-1.6.0.sql
@@ -0,0 +1,5 @@
+SELECT 'Upgrading Sentry store schema from 1.5.0 to 1.6.0' AS ' ';
+
+UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.6.0', VERSION_COMMENT='Sentry release version 1.6.0' WHERE VER_ID=1;
+
+SELECT 'Finish upgrading Sentry store schema from 1.5.0 to 1.6.0' AS ' ';
