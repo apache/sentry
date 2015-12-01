58cb426535bb4e6db9dc4b7e13f952b2468acfd9
hahao <hao.hao@cloudera.com>
2015-11-30 18:02:00 -0800

    CLOUDERA-BUILD: CDH-31396: Update post_commit hook scripts to disable tests.
    Change-Id: I06c1069851ed81e4e5d0fed5d6b2d1cf1fc85101
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/9618
    Tested-by: jenkins
    Reviewed-by: Hao Hao <hao.hao@cloudera.com>

--- cloudera/post_commit_hook.sh
+++ cloudera/post_commit_hook.sh
@@ -8,4 +8,5 @@ source /mnt/toolchain/toolchain.sh
 find . -name test-classes | grep target/test-classes | xargs rm -rf
 
 mvn clean compile package -DskipTests=true
-mvn test
+# Disable tests now, need to enable a subset of tests in future.
+# mvn test
