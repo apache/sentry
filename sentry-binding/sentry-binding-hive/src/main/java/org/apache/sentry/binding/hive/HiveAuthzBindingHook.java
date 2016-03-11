0619d8a1a0860674bae2fcc035148ce3d23d009f
Sravya Tirukkovalur <sravya@cloudera.com>
2016-03-10 17:18:03 -0800

    SENTRY-922: INSERT OVERWRITE DIRECTORY permission not working correctly (Yibing Shi, Reviewed by: Sravya Tirukkovalur)
    Change-Id: I9bc565506e544ee747a7ce5f7cc9b84a56786ec2

--- sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/HiveAuthzBindingHook.java
+++ sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/HiveAuthzBindingHook.java
@@ -30,6 +30,8 @@ import java.util.Set;
 import java.util.Arrays;
 
 import com.google.common.base.Preconditions;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.JavaUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
@@ -368,6 +370,11 @@ public class HiveAuthzBindingHook extends AbstractSemanticAnalyzerHook {
     try {
       HiveConf conf = SessionState.get().getConf();
       String warehouseDir = conf.getVar(ConfVars.METASTOREWAREHOUSE);
+      Path warehousePath = new Path(warehouseDir);
+      if (warehousePath.isAbsoluteAndSchemeAuthorityNull()) {
+        FileSystem fs = FileSystem.get(conf);
+        warehouseDir = fs.makeQualified(warehousePath).toUri().toString();
+      }
       return new AccessURI(PathUtils.parseURI(warehouseDir, uri, isLocal));
     } catch (Exception e) {
       throw new SemanticException("Error parsing URI " + uri + ": " +
