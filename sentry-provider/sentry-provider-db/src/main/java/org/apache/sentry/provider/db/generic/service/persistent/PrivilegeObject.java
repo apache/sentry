4bfe0be4d94b268961e4a2fa12e99df8efe1197f
Colin Ma <colin@apache.org>
2016-04-26 09:51:19 +0800

    SENTRY-1215: Sentry's db provider makes privileges case insensitive.(Ashish K Singh via Colin Ma)

--- sentry-provider/sentry-provider-db/src/main/java/org/apache/sentry/provider/db/generic/service/persistent/PrivilegeObject.java
+++ sentry-provider/sentry-provider-db/src/main/java/org/apache/sentry/provider/db/generic/service/persistent/PrivilegeObject.java
@@ -197,7 +197,7 @@ public class PrivilegeObject {
           }
           @Override
           public String getName() {
-            return authorizable.getName().toLowerCase();
+            return authorizable.getName();
           }
         });
       }
