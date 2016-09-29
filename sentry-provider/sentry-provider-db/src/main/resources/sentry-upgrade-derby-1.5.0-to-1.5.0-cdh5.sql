ac51b39546a42c9f9fdb193cd17d3458bdb801e6
Sravya Tirukkovalur <sravya@apache.org>
2016-09-29 14:05:20 -0700

    CDH-45368: Update CDH schema scripts for Sentry-1365 (Sravya Tirukkovalur)
    Change-Id: If1848415b99329485c166b35b4c7c0cce483ca21
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/16277
    Tested-by: Jenkins User
    Reviewed-by: Sravya Tirukkovalur <sravya@cloudera.com>

--- sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-derby-1.5.0-to-1.5.0-cdh5.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-derby-1.5.0-to-1.5.0-cdh5.sql
@@ -1,2 +1,5 @@
+RUN '007-SENTRY-872.derby.sql';
+
 -- Version update
+
 UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.5.0-cdh5', VERSION_COMMENT='Sentry release version 1.5.0-cdh5' WHERE VER_ID=1;
--- sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-oracle-1.5.0-to-1.5.0-cdh5.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-oracle-1.5.0-to-1.5.0-cdh5.sql
@@ -1,4 +1,5 @@
 SELECT 'Upgrading Sentry store schema from 1.5.0 to 1.5.0-cdh5' AS Status from dual;
+@007-SENTRY-872.oracle.sql;
 
 UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.5.0-cdh5', VERSION_COMMENT='Sentry release version 1.5.0-cdh5' WHERE VER_ID=1;
 SELECT 'Finished upgrading Sentry store schema from 1.5.0 to 1.5.0-cdh5' AS Status from dual;
