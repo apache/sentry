48ec01dd7db3d345976db9a3ea8c60162c72501e
Alexander Kolbasov <akolb@cloudera.com>
2017-03-06 13:18:07 -0800

    SENTRY-1569: Upgrading SQL scripts for persist Perm/Path change - added missing files (Kalyan Kumar Kalvagadda, Reviewed by: Hao Hao)

---
+++ sentry-provider/sentry-provider-db/src/main/resources/008-SENTRY-1569.postgres.sql
@@ -0,0 +1,17 @@
+-- Table `SENTRY_PERM_CHANGE` for classes [org.apache.sentry.provider.db.service.model.MSentryPermChange]
+CREATE TABLE "SENTRY_PERM_CHANGE"
+(
+    "CHANGE_ID" bigint NOT NULL,
+    "CREATE_TIME_MS" bigint NOT NULL,
+    "PERM_CHANGE" VARCHAR(4000) NOT NULL,
+    CONSTRAINT "SENTRY_PERM_CHANGE_PK" PRIMARY KEY ("CHANGE_ID")
+);
+
+-- Table `SENTRY_PATH_CHANGE` for classes [org.apache.sentry.provider.db.service.model.MSentryPathChange]
+CREATE TABLE "SENTRY_PATH_CHANGE"
+(
+    "CHANGE_ID" bigint NOT NULL,
+    "CREATE_TIME_MS" bigint NOT NULL,
+    "PATH_CHANGE" VARCHAR(4000) NOT NULL,
+    CONSTRAINT "SENTRY_PATH_CHANGE_PK" PRIMARY KEY ("CHANGE_ID")
+);
--- sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-derby-1.7.0-to-1.8.0.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-derby-1.7.0-to-1.8.0.sql
@@ -1,5 +1,6 @@
 RUN '006-SENTRY-711.derby.sql';
 RUN '007-SENTRY-1365.derby.sql';
+RUN '008-SENTRY-1569.derby.sql';
 
 -- Version update
 UPDATE SENTRY_VERSION SET SCHEMA_VERSION='1.8.0', VERSION_COMMENT='Sentry release version 1.8.0' WHERE VER_ID=1;
