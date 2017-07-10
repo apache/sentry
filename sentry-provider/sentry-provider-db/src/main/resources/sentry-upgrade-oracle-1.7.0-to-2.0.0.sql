c8d0f24a23186eac015292214d025ac7d9e9e415
Kalyan Kumar Kalvagadda <kkalyan@cloudera.com>
2017-07-10 17:46:53 -0500

    SENTRY-1828 Rename version in sentry-ha-redesign branch to 2.0.0-SNAPSHOT(kalyan kumar kalvagadda, Reviewed by Na Li, Alexander Kolbasov)

---
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-oracle-1.7.0-to-2.0.0.sql
@@ -0,0 +1,9 @@
+SELECT 'Upgrading Sentry store schema from 1.7.0 to 2.0.0' AS Status from dual;
+@006-SENTRY-711.oracle.sql;
+@007-SENTRY-1365.oracle.sql;
+@008-SENTRY-1569.oracle.sql;
+@009-SENTRY-1805.oracle.sql;
+
+UPDATE SENTRY_VERSION SET SCHEMA_VERSION='2.0.0', VERSION_COMMENT='Sentry release version 2.0.0' WHERE VER_ID=1;
+
+SELECT 'Finished upgrading Sentry store schema from 1.7.0 to 2.0.0' AS Status from dual;
---
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-postgres-1.7.0-to-2.0.0.sql
@@ -0,0 +1,9 @@
+SELECT 'Upgrading Sentry store schema from 1.7.0 to 2.0.0';
+\i 006-SENTRY-711.postgres.sql;
+\i 007-SENTRY-1365.postgres.sql;
+\i 008-SENTRY-1569.postgres.sql;
+\i 009-SENTRY-1805.postgres.sql;
+
+UPDATE "SENTRY_VERSION" SET "SCHEMA_VERSION"='2.0.0', "VERSION_COMMENT"='Sentry release version 2.0.0' WHERE "VER_ID"=1;
+
+SELECT 'Finished upgrading Sentry store schema from 1.7.0 to 2.0.0';
