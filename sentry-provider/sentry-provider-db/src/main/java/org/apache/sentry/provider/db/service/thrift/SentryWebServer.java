9c28d202a8eb692ef58c76f2a9e145c6d912d183
Donghui Xu <xu.donghui@zte.com.cn>
2017-07-24 15:21:12 +0800

    SENTRY-1844 - When setting web authentication type to none, sentry fails to start  - Reviewed by: Dapeng Sun.
    Re-applied by Sergio Pena.

--- sentry-provider/sentry-provider-db/src/main/java/org/apache/sentry/provider/db/service/thrift/SentryWebServer.java
+++ sentry-provider/sentry-provider-db/src/main/java/org/apache/sentry/provider/db/service/thrift/SentryWebServer.java
@@ -122,8 +122,8 @@ public class SentryWebServer {
     contextHandlerCollection.setHandlers(new Handler[]{contextHandler, servletContextHandler});
 
     String authMethod = conf.get(ServerConfig.SENTRY_WEB_SECURITY_TYPE);
-    if (!ServerConfig.SENTRY_WEB_SECURITY_TYPE_NONE.equals(authMethod)) {
-      /*
+    if (!ServerConfig.SENTRY_WEB_SECURITY_TYPE_NONE.equalsIgnoreCase(authMethod)) {
+      /**
        * SentryAuthFilter is a subclass of AuthenticationFilter and
        * AuthenticationFilter tagged as private and unstable interface:
        * While there are not guarantees that this interface will not change,
