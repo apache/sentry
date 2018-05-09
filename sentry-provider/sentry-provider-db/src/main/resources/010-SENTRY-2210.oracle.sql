e4791a029a27506afa4caf37552041216ad0a43b
lina.li <lina.li@cloudera.com>
2018-05-09 11:04:49 -0500

    SENTRY-2219: Create index AUTHZ_PATH_FK_IDX at table AUTHZ_PATH only when it does not exist for Oracle (Na Li, reviewed by Rajan Santhanam, Sergio Pena, Kalyan Kumar Kalvagadda, Arjun Mishra)

--- sentry-provider/sentry-provider-db/src/main/resources/010-SENTRY-2210.oracle.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/010-SENTRY-2210.oracle.sql
@@ -1,2 +1,15 @@
 -- create index for foreign key AUTHZ_OBJ_ID
-CREATE INDEX "AUTHZ_PATH_FK_IDX" ON "AUTHZ_PATH" ("AUTHZ_OBJ_ID");
+set serveroutput on
+declare
+  already_exists  exception;
+  columns_indexed exception;
+  pragma exception_init( already_exists, -955 );
+  pragma exception_init(columns_indexed, -1408);
+begin
+  execute immediate 'CREATE INDEX "AUTHZ_PATH_FK_IDX" ON "AUTHZ_PATH" ("AUTHZ_OBJ_ID")';
+  dbms_output.put_line( 'created' );
+exception
+  when already_exists or columns_indexed then
+  dbms_output.put_line( 'skipped' );
+end;
+/
