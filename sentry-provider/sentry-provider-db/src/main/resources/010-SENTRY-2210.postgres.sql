3d062f39ce6a047138660a7b3d0024bde916c5b4
amishra <amishra@cloudera.com>
2018-08-10 21:54:45 -0500

    SENTRY-2333: Create index AUTHZ_PATH_FK_IDX at table AUTHZ_PATH for Postgres only when it does not exist (Arjun Mishra reviewed by Na Li)
    Change-Id: I69caafa59d902d3231576f177d38a513c09a39f5

--- sentry-provider/sentry-provider-db/src/main/resources/010-SENTRY-2210.postgres.sql
+++ sentry-provider/sentry-provider-db/src/main/resources/010-SENTRY-2210.postgres.sql
@@ -1,2 +1,11 @@
 -- create index for foreign key AUTHZ_OBJ_ID
-CREATE INDEX "AUTHZ_PATH_FK_IDX" ON "AUTHZ_PATH" USING btree ("AUTHZ_OBJ_ID");
+DO $BLOCK$
+BEGIN
+    BEGIN
+        CREATE INDEX "AUTHZ_PATH_FK_IDX" ON "AUTHZ_PATH"( "AUTHZ_OBJ_ID" );
+    EXCEPTION
+        WHEN duplicate_table
+        THEN RAISE NOTICE 'index ''AUTHZ_PATH_FK_IDX '' on ''AUTHZ_PATH'' already exists, skipping';
+    END;
+END;
+$BLOCK$;
