b31210701ab73eda7cd0ac53097f9963c3bc0e64
lina.li <lina.li@cloudera.com>
2018-09-19 22:33:10 -0500

    SENTRY-2407: SentrySchemaInfo and SQL scripts do not have the new 2.2.0 version (Na Li, reviewed by Sergio Pena)

---
+++ sentry-provider/sentry-provider-db/src/main/resources/sentry-upgrade-oracle-2.1.0-to-2.2.0.sql
@@ -0,0 +1,6 @@
+SELECT 'Upgrading Sentry store schema from 2.1.0 to 2.2.0' AS Status from dual;
+
+
+UPDATE SENTRY_VERSION SET SCHEMA_VERSION='2.2.0', VERSION_COMMENT='Sentry release version 2.2.0' WHERE VER_ID=1;
+
+SELECT 'Finished upgrading Sentry store schema from 2.010 to 2.2.0' AS Status from dual;
